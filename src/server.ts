cdda27a4392b901aaf0e286eff75ddddd007c0a4
Vinicius Xavier <vinicius-rodrigues2000@hotmail.com>
2024-07-28 13:51:39 -0300

    chore: Add unit tests

--- src/server.ts
+++ src/server.ts
@@ -7,14 +7,20 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { buildSchema } from "type-graphql";
 import { SuppliersResolver } from "./resolvers/suppliers-resolver";
 
-async function bootstrap() {
+export let server: ApolloServer | null = null;
+
+export async function bootstrap(port?: number) {
   const schema = await buildSchema({
     resolvers: [SuppliersResolver],
     emitSchemaFile: path.resolve(__dirname, "schema.gql"),
   });
-  const server = new ApolloServer({ schema });
-  const port = process.env.PORT ? Number.parseInt(process.env.PORT) : 4000;
-  const { url } = await startStandaloneServer(server, { listen: { port } });
+  server = new ApolloServer({ schema });
+  const actualPort =
+    port || (process.env.PORT ? Number.parseInt(process.env.PORT) : 4000);
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: actualPort },
+  });
 
   console.log(`🚀 Server listening at: ${url}`);
 }
---
+++ tests/suppliers-resolver.test.ts
@@ -0,0 +1,42 @@
+import { server, bootstrap } from "../src/server";
+import request from "supertest";
+
+const port = 4001;
+
+beforeAll(async () => {
+  await bootstrap(port);
+}, 10000);
+
+afterAll(async () => {
+  await server?.stop();
+});
+
+describe("Suppliers Resolver", () => {
+  it("fetches suppliers based on consumption", async () => {
+    const response = await request(`http://localhost:${port}`)
+      .post("/")
+      .send({
+        query: `
+          query($consumption: Int!) {
+            suppliers(consumption: $consumption) {
+              id
+              name
+              logo
+              state
+              costPerKwh
+              minKwh
+              totalClients
+              averageRating
+            }
+          }
+        `,
+        variables: {
+          consumption: 10000,
+        },
+      });
+
+    expect(response.body.errors).toBeUndefined();
+    expect(response.body.data.suppliers).toBeInstanceOf(Array);
+    expect(response.body.data.suppliers.length).toBeGreaterThan(0);
+  });
+});
